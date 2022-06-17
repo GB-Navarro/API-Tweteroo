abd4eee0b52f0ca9040e6dc1313b270ace6a047e
GB-Navarro <gbnavarro@usp.br>
2022-06-17 14:01:45 -0300

    Created sign-up and tweets routes

--- server.js
+++ server.js
@@ -3,9 +3,34 @@ import cors from "cors";
 
 const app = express();
 
+let user = {}
+let tweetsArray = []
+
+app.use(express.json());
 app.use(cors());
-app.get("/", (request, response) => {
-    response.send("Hello World!!");
+
+app.post("/sign-up", (request, response) => {
+    user = request.body;
+    console.log(user);
+    response.send("Ok!");
+});
+app.post("/tweets", (request, response) => {
+    tweetsArray.push(request.body);
+    console.log(tweetsArray);
+    response.send("Ok!");
+})
+app.get("/tweets", (request, response) => {
+    if(tweetsArray.length > 10){
+        let lastTweets = []
+        let aux = 0;
+        for(let i = (tweetsArray.length - 11); aux != 10; i++){
+            lastTweets.push(tweetsArray[i]);
+            aux += 1;
+        }
+        response.send(lastTweets);
+    }else{
+        response.send(tweetsArray);
+    }
 });
 
 app.listen(5000);
